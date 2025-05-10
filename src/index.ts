6da1f2ccdb0ab306237b1d947764d6549744034d
FireGamer3 <gm31334@gmail.com>
2025-05-10 12:10:26 -0500

    Adds extra exports to allow for more use cases

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { default as Server } from '@/classes/server';
+export { default as Client } from '@/classes/client';
+export { default as Field } from '@/classes/builders/field';
 export { default as ScreenBuilder } from '@/classes/builders/screenBuilder';
 export * as Types from '@/types/index';
 export * as Constants from '@/util/constants';
